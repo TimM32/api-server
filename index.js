33af31a753ae7583955bae6adbe92f38f7c05b89
Tim Maupin <timmaupin@live.com>
2023-05-18 11:35:26 -0500

    format code and clean up the visual

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const { start } = require('./src/server');
 const PORT = process.env.PORT || 5002;
 
 sequelizeDatabase.sync()
-.then(() => {
+  .then(() => {
     console.log('Connection Complete');
     start(PORT);
-})
-.catch(e => console.error(e));
+  })
+  .catch(e => console.error(e));
 
 
